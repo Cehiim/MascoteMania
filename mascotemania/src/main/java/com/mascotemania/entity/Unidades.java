e8bc4866927e3083133e25832db217146fc6125d
Cesar Hideki Imai <116912049+Cehiim@users.noreply.github.com>
2024-05-29 02:38:04 -0300

    Create Unidades.java

---
+++ mascotemania/src/main/java/com/mascotemania/entity/Unidades.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+
+public class Unidades {
+    private HashMap<String, Integer> unidades;
+
+    public Unidades() {
+        unidades = new HashMap<>();
+    }
+
+    public void listarUnidadesEndereco() {
+        System.out.println("Unidades:");
+        for (String unidade : unidades.keySet()) {
+            String endereco; 
+            int id;
+            if (unidade.equalsIgnoreCase("Unidade X")) {
+                endereco = "Rua ABC, 123";
+                id = 1;
+            } else if (unidade.equalsIgnoreCase("Unidade Y")) {
+                endereco = "Rua DEF, 456";
+                id = 2;
+            } else {
+                id = -1;
+                endereco = "Unidade indisponível";
+            }
+            System.out.println(id + " - " + unidade + " - Endereço: " + endereco);
+        }
+    }
+
+    public HashMap<String, Integer> getUnidades() {
+        return  this.unidades;
+    }
+
+    public void listarUnidades() {
+        System.out.println("Unidades:");
+        for (String unidade : unidades.keySet()) {
+            System.out.println(unidade + ": " + unidades.get(unidade));
+        }
+    }
+}
