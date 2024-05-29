96a9b9e7c89713dd918a71b320f040dbecb8b509
Cesar Hideki Imai <116912049+Cehiim@users.noreply.github.com>
2024-05-29 02:33:52 -0300

    Update Agendamento.java

--- mascotemania/src/main/java/com/mascotemania/entity/Agendamento.java
+++ mascotemania/src/main/java/com/mascotemania/entity/Agendamento.java
@@ -4,48 +4,48 @@ import java.util.ArrayList;
 
 public class Agendamento {
     private String id;
-    private String pet;
-    private String local;
+    private Pet pet;
+    private String unidade;
     private ArrayList<String> servicos;
     private String data;
     private String horario;
     private float preco;
-  
 
-    public Agendamento(String id, String pet, String local, String data, String horario, float preco) {
+
+    public Agendamento(String id, Pet pet, String unidade, String data, String horario, float preco) {
         this.id = id;
         this.pet = pet;
-        this.local = local;
+        this.unidade = unidade;
         this.servicos = new ArrayList<String>();
         this.data = data;
         this.horario = horario;
         this.preco = preco;
     }
-    
+
     public String getId() {
         return id;
     }
-    public void setId(String id) {
-        this.id = id;
-    }
-  
-    public String getPet() {
+
+    public Pet getPet() {
         return pet;
     }
-    public void setPet(String pet) {
+    
+    public void setPet(Pet pet) {
         this.pet = pet;
     }
-  
-    public String getLocal() {
-        return local;
+
+    public String getUnidade() {
+        return unidade;
     }
-    public void setLocal(String local) {
-        this.local = local;
+    
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
     }
-  
+
     public ArrayList<String> getServicos() {
         return servicos;
     }
+    
     public void addServicos(String servico) {
         servicos.add(servico);
     }
@@ -53,6 +53,7 @@ public class Agendamento {
     public String getData() {
         return data;
     }
+    
     public void setData(String data) {
         this.data = data;
     }
@@ -60,6 +61,7 @@ public class Agendamento {
    public String getHorario() {
         return horario;
     }
+    
     public void setHorario(String horario) {
         this.horario = horario;
     }
@@ -67,6 +69,7 @@ public class Agendamento {
     public float getPreco() {
         return preco;
     }
+    
     public void setPreco(float preco) {
         this.preco = preco;
     }
