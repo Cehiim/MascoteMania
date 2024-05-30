b4025f2392d7937eb2d836e1a3460c20f51263a4
Cesar Hideki Imai <116912049+Cehiim@users.noreply.github.com>
2024-05-29 21:21:52 -0300

    Update AgendaServicoBoundary.java

--- mascotemania/src/main/java/com/mascotemania/boundary/AgendaServicoBoundary.java
+++ mascotemania/src/main/java/com/mascotemania/boundary/AgendaServicoBoundary.java
@@ -1,56 +1,49 @@
-// package com.mascotemania.boundary;
-// import com.mascotemania.entity.Cliente;
-// import com.mascotemania.entity.Agendamento;
-// import com.mascotemania.entity.Pet;
-// import com.mascotemania.entity.Servicos;
-// import com.mascotemania.entity.Unidades;
+package com.mascotemania.boundary;
 
-// import java.util.Scanner;
+import com.mascotemania.control.AgendaServicoControl;
+import com.mascotemania.entity.Cliente; 
+import com.mascotemania.entity.Pet;
+import com.mascotemania.entity.Servicos;
+import java.util.Scanner;
 
-// public class AgendaServicoBoundary {
-//   public AgendaServicoBoundary() {
-//   }
+public class AgendaServicoBoundary {
+    public AgendaServicoBoundary() {}
 
-// //   public void agendaServico(Cliente cliente, Scanner scanner, Pet pet, Agendamento agendamento, Servicos servico, Unidades unidade) {
-// //     System.out.println("---Agendar Serviço---");
-    
-// //     System.out.println("Insira o ID do pet.");
-// //     cliente.getPets().forEach(pet -> {System.out.println(pet.toString());});
-// //     int id_pet = scanner.nextInt();
+    public void agendaServico(Cliente cliente, Scanner scanner) {
+        System.out.println("Para qual Pet deseja agendar serviço? Insira o ID.");
+        cliente.getPets().forEach(pet -> {
+            System.out.println(pet.getId()
+            + " - " + pet.getNome() 
+            + " - " + pet.getTipo());
+        });
+        
+        int id = scanner.nextInt(); 
 
-// //     System.out.println("Insira o ID da unidade.");
-// //     unidade.listarUnidades();
-// //     int id_unidade = scanner.nextInt();
+        System.out.println("Qual tipo de serviço? Insira o ID."); 
+        System.out.println("1 - Remoção de subpelos - 30\n2 - Tosa tesoura - 30\n3 - Tosa higiênica - 40\n4 - Tosa máquina - 50\n5 - Tosa stripping -60");
+        int servico = scanner.nextInt();
 
-// //     System.out.println("Insira o ID do servico.");
-// //     servico.listarServicos();
-// //     int id_servico = scanner.nextInt();
+        System.out.println("Qual dia deseja realizar o seu serviço? Insira no formato: dia/mês - hora");
+        Scanner scanner2 = new Scanner(System.in);
+        String data = scanner2.nextLine();
 
-// //     System.out.println("Insira a data do agendamento.");
-// //     String data = scanner.next();
+        AgendaServicoControl agendaServicoControl = new AgendaServicoControl();
+        int retorno = agendaServicoControl.agendaServico(cliente, id, servico, data);
 
-// //     System.out.println("Insira o horário do agendamento");
-// //     String horario = scanner.next();
-// /*
-//     AgendaServicoControl agendaServicoControl = new AgendaServicoControl();
-//     int retorno = agendaServicoControl.agendaServico(cliente, id_pet, id_unidade, id_servico, data, horario);
-
-//     if (retorno == 200) {
-//         System.out.println("Agendamento realizado");
-//         System.out.println("Pedidos e seus status atuais: ");
-//         cliente.getAgendamentos().forEach(agendamento -> {
-//             System.out.println(agendamento.getId()
-//                     + " - " + agendamento.getPet()
-//                     + " - " + agendamento.getUnidade()
-//                     + " - " + agendamento.getServicos()
-//                     + " - " + agendamento.getData()
-//                     + " - " + agendamento.getHorario()
-//                     + " - " + agendamento.getPreco());
-//         });
-//         System.out.println();
-//         } else {
-//             System.out.println("Não foi possível realizar o agendamento");
-//         }*/
-//     }
-// }
-
+        if (retorno == 200) {
+            System.out.println("Agendamento realizado com sucesso"); 
+            System.out.println("Seus agendamentos: ");
+            cliente.getAgendamentos().forEach(agendamento -> {
+                System.out.println(agendamento.getId()
+                + " - " + agendamento.getPet().getNome() 
+                + " - " + agendamento.getServicos()
+                + " - " + agendamento.getData() 
+                + " - " + agendamento.getPreco());
+            });
+        } else if (retorno == 404) {
+            System.out.println("O ID do servico desejado não existe.");
+        } else if (retorno == 409) {
+            System.out.println("O ID do pet desejado não existe");
+        }
+    }
+}
